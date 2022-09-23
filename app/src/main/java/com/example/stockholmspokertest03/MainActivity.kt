20b6e45baa2c19f6544174a0bbeb29bbf7b65ed6
Jonas Sjogren <jonas.sjogren@iths.se>
2022-09-23 18:35:42 +0200

    Many views randomly spread out over the layout

--- app/src/main/java/com/example/stockholmspokertest03/MainActivity.kt
+++ app/src/main/java/com/example/stockholmspokertest03/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,31 +17,126 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView = ImageView(this)
+        //val imageView01 = ImageView(this)
+        val imageViewClub2 = ImageView(this)
+        val imageViewClub3 = ImageView(this)
+        val imageViewClub4 = ImageView(this)
+        val imageViewClub5 = ImageView(this)
+        val imageViewClub6 = ImageView(this)
+        val imageViewClub7 = ImageView(this)
+        val imageViewClub8 = ImageView(this)
+        val imageViewClub9 = ImageView(this)
+        val imageViewClub10 = ImageView(this)
+        val imageViewClubJ = ImageView(this)
+        val imageViewClubQ = ImageView(this)
+        val imageViewClubK = ImageView(this)
+        val imageViewClubA = ImageView(this)
+        val imageViewDiamond2 = ImageView(this)
+        val imageViewDiamond3 = ImageView(this)
+        val imageViewDiamond4 = ImageView(this)
+
+
 
         // setting height and width of imageview
-        imageView.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub2.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub3.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub4.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub5.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub6.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub7.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub8.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub9.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClub10.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClubJ.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClubQ.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClubK.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewClubA.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewDiamond2.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewDiamond3.layoutParams= LinearLayout.LayoutParams(400, 400)
+        imageViewDiamond4.layoutParams= LinearLayout.LayoutParams(400, 400)
         //imageView.x = 200F // setting margin from left
         //imageView.y = 850F // setting margin from top
 
         // accessing our custom image which we added in drawable folder
-        val imgResId = R.drawable.img
-        var resId = imgResId
+
+        //val imgResId = R.drawable.playing_card_club_2
+       // var resId = imgResId
+
 
         // button onClick listener
         val button = findViewById<Button>(R.id.button)
         button?.setOnClickListener{
-            imageView.setImageResource(resId)
+            imageViewClub2.setImageResource(R.drawable.playing_card_club_2)
+            imageViewClub3.setImageResource(R.drawable.playing_card_club_3)
+            imageViewClub4.setImageResource(R.drawable.playing_card_club_4)
+            imageViewClub5.setImageResource(R.drawable.playing_card_club_5)
+            imageViewClub6.setImageResource(R.drawable.playing_card_club_6)
+            imageViewClub7.setImageResource(R.drawable.playing_card_club_7)
+            imageViewClub8.setImageResource(R.drawable.playing_card_club_8)
+            imageViewClub9.setImageResource(R.drawable.playing_card_club_9)
+            imageViewClub10.setImageResource(R.drawable.playing_card_club_10)
+            imageViewClubJ.setImageResource(R.drawable.playing_card_club_j)
+            imageViewClubQ.setImageResource(R.drawable.playing_card_club_q)
+            imageViewClubK.setImageResource(R.drawable.playing_card_club_k)
+            imageViewClubA.setImageResource(R.drawable.playing_card_club_a)
+            imageViewDiamond2.setImageResource(R.drawable.playing_card_diamond_2)
+            imageViewDiamond3.setImageResource(R.drawable.playing_card_diamond_3)
+            imageViewDiamond4.setImageResource(R.drawable.playing_card_diamond_4)
 
-            placeView(imageView)
-            rotateView(imageView)
+            placeView(imageViewClub2)
+            rotateView(imageViewClub2)
+            placeView(imageViewClub3)
+            rotateView(imageViewClub3)
+            placeView(imageViewClub4)
+            rotateView(imageViewClub4)
+            placeView(imageViewClub5)
+            rotateView(imageViewClub5)
+            placeView(imageViewClub6)
+            rotateView(imageViewClub6)
+            placeView(imageViewClub7)
+            rotateView(imageViewClub7)
+            placeView(imageViewClub8)
+            rotateView(imageViewClub8)
+            placeView(imageViewClub9)
+            rotateView(imageViewClub9)
+            placeView(imageViewClub10)
+            rotateView(imageViewClub10)
+            placeView(imageViewClubJ)
+            rotateView(imageViewClubJ)
+            placeView(imageViewClubQ)
+            rotateView(imageViewClubQ)
+            placeView(imageViewClubK)
+            rotateView(imageViewClubK)
+            placeView(imageViewClubA)
+            rotateView(imageViewClubA)
+            placeView(imageViewDiamond2)
+            rotateView(imageViewDiamond2)
+            placeView(imageViewDiamond3)
+            rotateView(imageViewDiamond3)
+            placeView(imageViewDiamond4)
+            rotateView(imageViewDiamond4)
 
         }
         // accessing our relative layout from activity_main.xml
         val layout = findViewById<RelativeLayout>(R.id.layout)
 
         // Add ImageView to LinearLayout
-        layout?.addView(imageView) // adding image to the layout
+        layout?.addView(imageViewClub2) // adding image to the layout
+        layout?.addView(imageViewClub3)
+        layout?.addView(imageViewClub4)
+        layout?.addView(imageViewClub5)
+        layout?.addView(imageViewClub6)
+        layout?.addView(imageViewClub7)
+        layout?.addView(imageViewClub8)
+        layout?.addView(imageViewClub9)
+        layout?.addView(imageViewClub10)
+        layout?.addView(imageViewClubJ)
+        layout?.addView(imageViewClubQ)
+        layout?.addView(imageViewClubK)
+        layout?.addView(imageViewClubA)
+        layout?.addView(imageViewDiamond2)
+        layout?.addView(imageViewDiamond3)
+        layout?.addView(imageViewDiamond4)
 
     }
 
