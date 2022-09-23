4c75b968e063b518c2b65bb0ae43131b94cf3e3e
Jonas Sjogren <jonas.sjogren@iths.se>
2022-09-23 10:59:43 +0200

    Första commit

---
+++ app/src/main/java/com/example/stockholmspokertest03/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.stockholmspokertest03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageView = ImageView(this)
+
+        // setting height and width of imageview
+        imageView.layoutParams= LinearLayout.LayoutParams(400, 400)
+        //imageView.x = 200F // setting margin from left
+        //imageView.y = 850F // setting margin from top
+
+        // accessing our custom image which we added in drawable folder
+        val imgResId = R.drawable.img
+        var resId = imgResId
+
+        // button onClick listener
+        val button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener{
+            imageView.setImageResource(resId)
+
+            placeView(imageView)
+            rotateView(imageView)
+
+        }
+        // accessing our relative layout from activity_main.xml
+        val layout = findViewById<RelativeLayout>(R.id.layout)
+
+        // Add ImageView to LinearLayout
+        layout?.addView(imageView) // adding image to the layout
+
+    }
+
+    fun placeView(view : ImageView){
+        var randomIntX = (10..700).random()
+        view.x = randomIntX.toFloat()
+        var randomIntY = (10..1600).random()
+        view.y = randomIntY.toFloat()
+    }
+
+    fun rotateView(view : ImageView) {
+        var randomInt = (0..360).random()
+        view.rotation = randomInt.toFloat()
+    }
+
+}
+
